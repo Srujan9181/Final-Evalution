280a560d87635cee669d15693ab392e6402ccb3d
Srujan9181 <gsrujan9181@.com>
2025-03-28 12:07:39 +0530

    final

--- src/Components/signup.jsx
+++ src/Components/signup.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './signup.css'
-import Logo from '../assets/Logo.png'
+import Logo from '../assets/logo.png'
 import Face from '../assets/Frame.png'
 import { useNavigate } from 'react-router-dom'
 
